c36211ac6fe5081ddd259c142c6bacd1fbd0c358
Carlos Barros <carlos_dbs@hotmail.com>
2022-02-15 15:13:41 -0300

    feat: adiciona ListTalkerController para rota get talker/
    Adiciona ListTalkerController para rota get que retorna uma
    array com todos os talkers cadastrados.

---
+++ controllers/ListTalkerController.js
@@ -0,0 +1,9 @@
+const ListTalkerController = (request, response) => response.status(200).json({ message: 'OK' });
+
+module.exports = ListTalkerController;
+
+/**
+ * @author Carlos Barros
+ * @since Desenvolvido em 2022-02-15 
+ * @version 1.0.0
+ */
--- routes.js
+++ routes.js
@@ -1,5 +1,15 @@
 const express = require('express');
 
+const ListTalkerController = require('./controllers/ListTalkerController');
+
 const router = express.Router();
 
+router.get('/talker', ListTalkerController);
+
 module.exports = router;
+
+/**
+ * @author Carlos Barros
+ * @since Desenvolvido em 2022-02-15 
+ * @version 1.0.0
+ */
