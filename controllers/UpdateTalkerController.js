d44921b39391965b08c3af924ce0b7b24d0ff6e0
Carlos Barros <carlos_dbs@hotmail.com>
2022-03-04 17:33:28 -0300

    refactor:add return em ensureAuthenticate e remove .end().

--- controllers/UpdateTalkerController.js
+++ controllers/UpdateTalkerController.js
@@ -8,9 +8,11 @@ const UpdateTalkerController = async (request, response) => {
   const listTalkerJSON = await readFile('./talker.json');
   let listTalker = JSON.parse(listTalkerJSON);
   const talkerFound = listTalker.find((talker) => talker.id === +id);
+  
   if (!talkerFound) return response.status(404).json({ message: 'Not Found!' });
 
   const updatedTalker = { ...talkerFound, name, age, talk };
+
   listTalker = [...listTalker.filter((talker) => talker.id !== +id), updatedTalker];
   const orderedTalker = listTalker
   .sort((firstTalker, secondTalker) => firstTalker.id - secondTalker.id);
--- middlewares/ensureAuthenticate.js
+++ middlewares/ensureAuthenticate.js
@@ -1,10 +1,10 @@
 const ensureAuthenticate = (request, response, next) => {
   const token = request.headers.authorization;
   if (!token) {
-    return response.status(401).json({ message: 'Token não encontrado' }).end();
+    return response.status(401).json({ message: 'Token não encontrado' });
   }
 
-  if (token.length < 16) response.status(401).json({ message: 'Token inválido' }).end();
+  if (token.length < 16) return response.status(401).json({ message: 'Token inválido' });
 
   return next();
 };
